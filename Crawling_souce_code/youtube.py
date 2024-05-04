c66be071d312dc01035c2e803dbab3069b283d8d
pigman0504 <129501849+pigman0504@users.noreply.github.com>
2024-05-05 02:15:25 +0700

    Second Commit Change
    기존에 댓글만 출력하는 기능에서 가장 좋아요가 많은/답글이 많은 기능을 출력하는 프로그램으로 변화시켰습니다

--- Crawling_souce_code/youtube.py
+++ Crawling_souce_code/youtube.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
 
 def get_all_comments(youtube_url):
     # 비디오 ID 추출
@@ -18,10 +17,14 @@ def get_all_comments(youtube_url):
         response = requests.get(url)
         data = response.json()
         
-        # 각 댓글의 텍스트 추출하여 리스트에 추가
+        # 각 댓글의 정보 추출하여 리스트에 추가
         for item in data['items']:
-            comment_text = item['snippet']['topLevelComment']['snippet']['textOriginal']
-            all_comments.append(comment_text)
+            comment_info = {
+                'text': item['snippet']['topLevelComment']['snippet']['textOriginal'],
+                'like_count': item['snippet']['topLevelComment']['snippet']['likeCount'],
+                'reply_count': item['snippet']['totalReplyCount']
+            }
+            all_comments.append(comment_info)
         
         # 다음 페이지 토큰이 있는 경우에만 다음 페이지 호출
         if 'nextPageToken' in data:
@@ -36,6 +39,17 @@ def get_all_comments(youtube_url):
 youtube_url = input("유튜브 링크를 입력하세요: ")
 # 모든 댓글 가져오기
 all_comments = get_all_comments(youtube_url)
-# 모든 댓글 출력
-for comment in all_comments:
-    print(comment)
+
+# 좋아요가 가장 많은 댓글 출력
+most_liked_comment = max(all_comments, key=lambda x: x['like_count'])
+print("좋아요가 가장 많은 댓글:")
+print(f"댓글 내용: {most_liked_comment['text']}")
+print(f"좋아요 개수: {most_liked_comment['like_count']}")
+print(f"답글 개수: {most_liked_comment['reply_count']}")
+
+# 답글이 가장 많은 댓글 출력
+most_replied_comment = max(all_comments, key=lambda x: x['reply_count'])
+print("\n답글이 가장 많은 댓글:")
+print(f"댓글 내용: {most_replied_comment['text']}")
+print(f"좋아요 개수: {most_replied_comment['like_count']}")
+print(f"답글 개수: {most_replied_comment['reply_count']}")
